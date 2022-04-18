20bf830f226132a681b08e5f06b8c92bb0b347d6
Nguyễn Hữu Thiêng <16521532@gm.uit.edu.vn>
2022-04-18 21:40:45 +0700

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,28 +1,35 @@
 import {
   Body,
   Controller,
+  Get,
+  Request,
   HttpStatus,
+  Param,
   Post,
   Res,
+  UseGuards,
   ValidationPipe,
-} from '@nestjs/common';
-import { BaseResponseData } from 'src/utils.common/utils.response.common/utils.base.response.common';
+} from "@nestjs/common";
+import { BaseResponseData } from "src/utils.common/utils.response.common/utils.base.response.common";
 
-import { UsersLoginDTO } from './users.dto/users.login.dto';
-import { UsersRegisterDTO } from './users.dto/users.register.dto';
-import { Users } from './users.entity/users.entity';
-import { UsersDetailResponse } from './users.response/users.detail.response';
-import { UsersService } from './users.service';
-import { Response } from 'express';
+import { UsersLoginDTO } from "./users.dto/users.login.dto";
+import { UsersRegisterDTO } from "./users.dto/users.register.dto";
+import { Users } from "./users.entity/users.entity";
+import { UsersDetailResponse } from "./users.response/users.detail.response";
+import { UsersService } from "./users.service";
+import { Response } from "express";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { UsersParamsDTO } from "./users.dto/users.params.dto";
+import { VerifyCodeDTO } from "./users.dto/user.verify.dto";
 
-@Controller('users')
+@Controller("users")
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  @Post('/register')
+  @Post("/register")
   async register(
     @Body(new ValidationPipe()) usersRegisterDTO: UsersRegisterDTO,
-    @Res() res: Response,
+    @Res() res: Response
   ): Promise<any> {
     let response: BaseResponseData = new BaseResponseData();
 
@@ -33,10 +40,10 @@ export class UsersController {
     return res.status(HttpStatus.OK).send(response);
   }
 
-  @Post('/login')
+  @Post("/login")
   async login(
     @Body(new ValidationPipe()) usersLoginDTO: UsersLoginDTO,
-    @Res() res: Response,
+    @Res() res: Response
   ): Promise<any> {
     let response: BaseResponseData = new BaseResponseData();
 
@@ -45,5 +52,29 @@ export class UsersController {
     response.setData(access_token);
 
     return res.status(HttpStatus.OK).send(response);
+  }
+
+  @Get("/detail/:id")
+  @UseGuards(JwtAuthGuard)
+  async getDetail(
+    @Param(new ValidationPipe()) usersParamsDTO: UsersParamsDTO,
+    @Res() res: Response,
+    @Request() req
+  ): Promise<any> {
+    return res
+      .status(200)
+      .send({ status: 200, message: "mesage", data: req.user });
+  }
+
+  @Post("/verify-code")
+  async verifyCode(
+    @Body(new ValidationPipe()) verifyCodeDTO: VerifyCodeDTO,
+    @Res() res: Response,
+    @Request() req
+  ): Promise<any> {
+    let user: Users = await this.usersService.verifyCode(verifyCodeDTO);
+    return res
+      .status(200)
+      .send({ status: 200, message: "success", data: verifyCodeDTO });
   }
 }
