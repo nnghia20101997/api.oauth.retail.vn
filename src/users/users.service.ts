ae6de3a004e8dd0892fbbc9eb0be9afd5f1056d9
Nguyễn Hữu Thiêng <16521532@gm.uit.edu.vn>
2022-04-18 21:41:03 +0700

    Update users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,23 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { InjectRepository } from '@nestjs/typeorm';
-import { ExceptionResponseDetail } from 'src/utils.common/utils.exception.common/utils.exception.common';
-import { Password } from 'src/utils.common/utils.password.common/utils.password.common';
-import { Repository } from 'typeorm';
-import { UsersLoginDTO } from './users.dto/users.login.dto';
-import { UsersRegisterDTO } from './users.dto/users.register.dto';
-import { Users } from './users.entity/users.entity';
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { InjectRepository } from "@nestjs/typeorm";
+import { ExceptionResponseDetail } from "src/utils.common/utils.exception.common/utils.exception.common";
+import { Password } from "src/utils.common/utils.password.common/utils.password.common";
+import { Repository } from "typeorm";
+import { VerifyCodeDTO } from "./users.dto/user.verify.dto";
+import { UsersLoginDTO } from "./users.dto/users.login.dto";
+import { UsersRegisterDTO } from "./users.dto/users.register.dto";
+import { Users } from "./users.entity/users.entity";
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(Users)
     private users: Repository<Users>,
-    private jwtService: JwtService,
+    private jwtService: JwtService
   ) {}
 
   async register(usersRegisterDTO: UsersRegisterDTO): Promise<Users> {
-    console.log(usersRegisterDTO)
     let userRegistered: Users = await this.users.findOne({
       phone: usersRegisterDTO.phone,
     });
@@ -26,23 +26,17 @@ export class UsersService {
       throw new HttpException(
         new ExceptionResponseDetail(
           HttpStatus.BAD_REQUEST,
-          'Số điện thoại đã được đăng ký !!!',
+          "Số điện thoại đã được đăng ký !!!"
         ),
-        HttpStatus.OK,
+        HttpStatus.OK
       );
     }
 
-    usersRegisterDTO.password = await Password.bcryptPassword(
-      usersRegisterDTO.password,
-    );
+    let newUserEntity = new Users(usersRegisterDTO);
 
-    const payload = {
-      username: usersRegisterDTO.phone,
-    };
+    await newUserEntity.setPassword(usersRegisterDTO.password);
 
-    usersRegisterDTO.access_token = await this.jwtService.sign(payload);
-
-    const user: Users = await this.users.create(usersRegisterDTO);
+    const user: Users = await this.users.create(newUserEntity);
 
     await this.users.save(user);
     return user;
@@ -57,22 +51,22 @@ export class UsersService {
       throw new HttpException(
         new ExceptionResponseDetail(
           HttpStatus.BAD_REQUEST,
-          'Số điện thoại chưa được đăng ký. Vui lòng kiểm tra lại !!!',
+          "Số điện thoại chưa được đăng ký. Vui lòng kiểm tra lại !!!"
         ),
-        HttpStatus.OK,
+        HttpStatus.OK
       );
     }
 
     // compare password
     let isValid = await Password.comparePassword(
       usersLoginDTO.password,
-      user.password,
+      user.password
     );
 
     if (!isValid) {
       throw new HttpException(
-        new ExceptionResponseDetail(HttpStatus.BAD_REQUEST, 'Sai mật khẩu !!!'),
-        HttpStatus.OK,
+        new ExceptionResponseDetail(HttpStatus.BAD_REQUEST, "Sai mật khẩu !!!"),
+        HttpStatus.OK
       );
     }
 
@@ -80,5 +74,40 @@ export class UsersService {
       phone: user.phone,
     };
     return { access_token: await this.jwtService.sign(payload) };
+  }
+
+  async findOneUser(phone: string): Promise<Users> {
+    let user: Users = await this.users.findOne({
+      phone: phone,
+    });
+
+    if (!user) {
+      throw new HttpException(
+        new ExceptionResponseDetail(
+          HttpStatus.UNAUTHORIZED,
+          "token không hợp lệ !!!"
+        ),
+        HttpStatus.OK
+      );
+    }
+    return user;
+  }
+
+  async verifyCode(verifyCodeDTO: VerifyCodeDTO): Promise<Users> {
+    let user: Users = await this.users.findOne({
+      phone: verifyCodeDTO.phone,
+      verify_code: verifyCodeDTO.verify_code,
+    });
+
+    if (!user) {
+      throw new HttpException(
+        new ExceptionResponseDetail(
+          HttpStatus.UNAUTHORIZED,
+          "token không hợp lệ !!!"
+        ),
+        HttpStatus.OK
+      );
+    }
+    return user;
   }
 }
